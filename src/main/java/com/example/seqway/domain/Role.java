647590d218a68cb92af756cedb48af07eb9b0ddf
Vasilii <cojocari.v88@gmail.com>
2019-09-02 15:14:14 +0300

    Initial commit

---
+++ src/main/java/com/example/seqway/domain/Role.java
@@ -0,0 +1,5 @@
+package com.example.seqway.domain;
+
+public enum Role {
+    USER;
+}
---
+++ src/main/java/com/example/seqway/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.example.seqway.repos;
+
+import com.example.seqway.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
